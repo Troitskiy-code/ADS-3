7e82efa3cec5579c9cd9f1e15382508304d7f8b1
Troitskiy-code <mrcheleng87@gmail.com>
2026-03-22 14:43:28 +0300

    Update alg3.1.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,15 +3,45 @@
 #include "alg.h"
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+    uint64_t max_val = num;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = 3 * num + 1;
+        }
+        if (num > max_val) {
+            max_val = num;
+        }
+    }
+    return max_val;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+    unsigned int length = 1;
+    while (num != 1) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        }
+        else {
+            num = 3 * num + 1;
+        }
+        length++;
+    }
+    return length;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
-                        uint64_t rbound) {
-  return 1;
+unsigned int seqCollatz(unsigned int* maxlen, uint64_t lbound, uint64_t rbound) {
+    unsigned int best_number = lbound;
+    *maxlen = collatzLen(lbound);
+
+    for (uint64_t current = lbound + 1; current <= rbound; ++current) {
+        unsigned int current_len = collatzLen(current);
+        if (current_len > *maxlen) {
+            *maxlen = current_len;
+            best_number = current;
+        }
+    }
+    return best_number;
 }
